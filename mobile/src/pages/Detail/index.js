401189af787f4d9b8bb6f44a140e37bb5ddbb845
Marcos <marcos.silva.arcanjo2@gmail.com>
2020-05-02 02:56:45 -0300

    mobile finalization

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,30 +1,65 @@
 import React from 'react';
 import {Feather} from '@expo/vector-icons';
-import {View,Text,TouchableOpacity,Image} from 'react-native';
+import {View,Text,TouchableOpacity,Image,Linking} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
+
+
 
 import styles from './styles'
 import logoImg from '../../assets/logo.png'
 
 export default function  Detail(){
+
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const incident = route.params.incidentData;
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso ${incident.tittle}`;
+
+
+    function navigateBack(){
+        navigation.goBack();
+    }
+
+    function sendMail(){
+        MailComposer.composeAsync({
+            subject: `Herói do caso: ${incident.tittle}`,
+            recipients: [incident.email],
+            body: message,
+        })
+
+    }
+
+    function sendWhatsapp(){
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
+    }
+
+
+
+
     return(
         <View style= {styles.container}>
              <View style={styles.header}>
                 <Image source={logoImg} />
                 
-                <TouchableOpacity onPress={()=>{}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#E82041"/>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.incident}>
-                <Text style={styles.incidentProperty}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Animal Perdido</Text>
+                <Text style={styles.incidentValue}>{incident.tittle}</Text>
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR',{style:'currency', currency: 'BRL'})
+                .format(incident.value)}</Text>
+
+                
 
             </View>
 
@@ -35,12 +70,12 @@ export default function  Detail(){
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={()=>{}}>
-                        <Text styles={styles.actionText}>WhatsApp</Text>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+                        <Text style={styles.actionText}>WhatsApp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={()=>{}}>
-                        <Text styles={styles.actionText}>E-mail</Text>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
+                        <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
             </View>
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Feather} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native';
 import {View, FlatList, Image, Text, TouchableOpacity,Button} from 'react-native';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.png'
 
@@ -9,17 +11,50 @@ import styles from './styles';
 
 export default function  Incidents(){
     const navigation = useNavigation();
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
 
-    function navigateToDetail(){
-        navigation.navigate('Detail');
+    const [page,setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    function navigateToDetail(incidentData){
+        navigation.navigate('Detail', {incidentData});
     }
+
+    async function loadIncidents(){
+        if (loading){
+            return;
+        }
+
+        if(total > 0 && incidents.length === total){
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('incidents',{
+            params: {page}
+        });
+
+    
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
+    }
+
+    useEffect(() =>{
+        loadIncidents();
+
+    },[])
+
 
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style = {styles.headerText}>
-                    Total de <Text style ={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style ={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
 
@@ -28,21 +63,25 @@ export default function  Incidents(){
 
             <FlatList
               style={styles.incidentList}
-              data={[1,2]}
-              keyExtractor={incident => String(incident)}
+              data={incidents}
+              keyExtractor={incident => String(incident.id)}
               showsVerticalScrollIndicator={false}
-              renderItem={()=>(
+              onEndReached={loadIncidents}
+              onEndReachedThreshold={0.2}
+              renderItem={({item: incident})=>(
                 <View style ={styles.incident}>
                     <Text style={styles.incidentProperty}>ONG:</Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
+                    <Text style={styles.incidentValue}>{incident.name}</Text>
 
                     <Text style={styles.incidentProperty}>CASO:</Text>
-                    <Text style={styles.incidentValue}>Animal Perdido</Text>
+                    <Text style={styles.incidentValue}>{incident.tittle}</Text>
 
                     <Text style={styles.incidentProperty}>VALOR:</Text>
-                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+                    <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+                        style: 'currency', currency: 'BRL'
+                         }).format(incident.value)}</Text>
 
-                    <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+                    <TouchableOpacity style={styles.detailsButton} onPress={()=>navigateToDetail(incident)}>
                         <Text style={styles.detailsButtonText}> Ver mais detalhes</Text>
                         <Feather name="arrow-right" size={16} color="#e02041" />
                     </TouchableOpacity>
