4d68b19f8f60ca0c20a6cd5239510fec721ffe6b
Marcos <marcos.silva.arcanjo2@gmail.com>
2020-05-23 00:36:25 -0300

    final module

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const cors = require('cors');
+const { errors} = require('celebrate')
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(cors(
 ));
 app.use(express.json()); //informa ao app que vamos usar json no corpo das req
 app.use(routes);
-
+app.use(errors());
 
 
 app.listen(3333);
